f10b3408c827286013fb6689166fb65f39417b7a
i-am-star <adannaya649@gmail.com>
2023-01-20 14:31:13 +0100

    working with loops

--- loops.c
+++ loops.c
@@ -1,10 +1,18 @@
-#include <stdio.h>
+ #include <stdio.h>
+
 int main() {
 	int age =
 		printf("How old are you? \n");
 	scanf("%d", &age);
 	while (age > 18) {
 		printf("Please proceed");
+	}
+
+	int count = 0;
+
+	while (count < 20) {
+		printf("welcome\n");
+		count = count + 1;
 	}
 	return 0;
 }

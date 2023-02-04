23782937c0037f55685669127287668ea36a9a88
i-am-star <adannaya649@gmail.com>
2023-02-04 23:34:48 +0100

    made few corrections

--- beginner_projects/mad_lips_game.c
+++ beginner_projects/mad_lips_game.c
@@ -3,10 +3,10 @@
 
 int main()
 {
- const char name[50];
- const int age;
- const char city[70];
-  const char occupation[80];
+  char name[50];
+  int age;
+  char city[70];
+   char occupation[80];
 
   printf("Enter your character's name: ");
   scanf("%s", name);
@@ -19,7 +19,7 @@ int main()
 
 printf("Once upon a time, in a very far place, there lived a girl called %s\n", name);
 printf(name, "is %d years old.\n", age);
-printf("Though she grew up in a small town,", name, "had big dreams of moving to %s\n", city);
-printf("When", name, "relocates to", city, "she will become a %s\n", occupation);
+printf("Though she grew up in a small town, yet she had big dreams of moving to %s\n", city);
+printf("When she relocates to her dream city, she will become a %s\n", occupation);
 return 0;
 }

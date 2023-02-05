5a5e0c4ae41304273909e0f7ae9402f217ee5504
i-am-star <adannaya649@gmail.com>
2023-02-05 14:06:42 +0100

    ..

--- learning_c/functions.c
+++ learning_c/functions.c
@@ -1,6 +1,7 @@
 // a collection of codes that performs a specific task 
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 int main()
 {
@@ -11,7 +12,11 @@ printf("then some subtraction\n");
 subNum();
 printf("Another addition\n");
 addSum(4, 4);
-
+printf("yet another addition\n");
+int result = sumNum(6,7);
+printf("Result = %d\n", result);
+int result1 = timesNum(5,10);
+printf("Result =%d", result1) ;
 return 0;
 }
 
@@ -41,5 +46,17 @@ void subNum()
 void addSum(int Num1, int Num2)
 {
     int result = Num1 + Num2;
-    printf("%d + %d = %d", Num1, Num2, Num1 + Num2);
+    printf("%d + %d = %d\n", Num1, Num2, Num1 + Num2);
+}
+//functions, parameters, and return types.
+int sumNum(int Num1, int Num2)
+{
+    int sum = Num1 + Num2;
+    return sum;
+}
+/* create a function that takes two numbers, performs the multiplication of two numbers and returns the result*/
+int timesNum(int Num3, int Num4)
+{
+    int sum1 = Num3 * Num4;
+    return sum1;
 }
